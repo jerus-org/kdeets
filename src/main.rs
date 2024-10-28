c3dd283dd0bb65f3a8260c443c8f48dc94a6a063
Jeremiah Russell <jerry@jrussell.ie>
2024-10-28 09:39:30 +0000

    feat(cli): add command-line interface with logging and verbosity options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+use clap_verbosity::Verbosity;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(flatten)]
+    logging: Verbosity,
+
+    /// The name of the crate
+    #[clap(short = 'k', long = "krate")]
+    krate: String,
+}
+
 fn main() {
+    let args = Cli::parse();
+
+    let mut builder = get_logging(args.logging.log_level_filter());
+    builder.init();
+
     println!("Hello, world!");
+}
+
+fn get_logging(level: log::LevelFilter) -> env_logger::Builder {
+    let mut builder = env_logger::Builder::new();
+
+    builder.filter(None, level);
+
+    builder.format_timestamp_secs().format_module_path(false);
+
+    builder
 }
