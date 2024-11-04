60685e22ae9beea63e50f78f96fb4aeb49258a7a
Jeremiah Russell <jerry@jrussell.ie>
2024-11-04 16:02:47 +0000

    refactor(crate_versions): improve output formatting and add default trait implementation
    Add default trait implementation for CrateVersions struct and improve output formatting by ensuring consistent newlines. Implement tests for various scenarios to ensure correct functionality.

--- src/crate_versions.rs
+++ src/crate_versions.rs
@@ -12,7 +12,7 @@ use tame_index::{
     IndexLocation, IndexUrl, KrateName, SparseIndex,
 };
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Default)]
 #[clap(author, version, about, long_about = None)]
 pub struct CrateVersions {
     #[clap(flatten)]
@@ -121,11 +121,14 @@ impl CrateVersions {
 
         if self.normal | self.all | self.key {
             output = format!(
-                "{}   {}",
+                "{}   {}\n",
                 output,
                 format!(
-                    "Highest normal version: {}\n",
-                    index_crate.highest_normal_version().unwrap().version
+                    "Highest normal version: {}",
+                    index_crate
+                        .highest_normal_version()
+                        .unwrap_or_else(|| index_crate.highest_version())
+                        .version
                 )
                 .blue()
             );
@@ -133,22 +136,18 @@ impl CrateVersions {
 
         if self.highest | self.all | self.key {
             output = format!(
-                "{}   {}",
+                "{}   {}\n",
                 output,
-                format!(
-                    "Highest version: {}\n",
-                    index_crate.highest_version().version
-                )
-                .green()
+                format!("Highest version: {}", index_crate.highest_version().version).green()
             );
         };
 
         if self.recent | self.all | self.key {
             output = format!(
-                "{}   {}",
+                "{}   {}\n",
                 output,
                 format!(
-                    "Most recent version: {}\n",
+                    "Most recent version: {}",
                     index_crate.most_recent_version().version
                 )
                 .yellow()
@@ -156,15 +155,263 @@ impl CrateVersions {
         };
 
         if self.list | self.all {
-            output = format!("{}\n   {}\n", output, "Yanked  Version   ".underlined());
-            for v in index_crate.versions.iter() {
-                let version = v.version.clone();
-                let yanked = if v.yanked { "Yes".red() } else { " No".green() };
+            const BASE_HEADER: &str = " Yanked  Version ";
 
-                output = format!("  {}     {}     {}\n", output, yanked, version);
+            let mut header = BASE_HEADER.to_string();
+
+            let rows = index_crate
+                .versions
+                .iter()
+                .map(|x| {
+                    format!(
+                        "   {}     {}",
+                        if x.yanked { "Yes".red() } else { " No".green() },
+                        x.version
+                    )
+                })
+                .collect::<Vec<String>>();
+
+            log::debug!("Rows: {:#?}!", rows);
+
+            let max_row = &rows
+                .iter()
+                .map(|x| {
+                    log::debug!("Line: `{}`, len: `{}`!", x, x.chars().count(),);
+                    x.len() - 12
+                })
+                .max()
+                .unwrap_or(BASE_HEADER.len());
+            log::debug!("Max row length: {}!", max_row);
+
+            while header.len() < *max_row {
+                header = format!("{} ", header);
             }
+            log::debug!("Output: {}!", output);
+            log::debug!("Header: {}!", header);
+
+            let rows = format!("   {}\n", rows.join("\n   "));
+
+            output = format!("{}   {}\n{}", output, header.underlined(), rows);
         }
 
         Ok(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use colorful::Colorful;
+    use rstest::fixture;
+
+    use crate::crate_versions::CrateVersions;
+
+    #[fixture]
+    fn header(#[default("some_crate")] name: &str) -> String {
+        let output = format!(
+            "\n {}",
+            format!("Crate versions for {}.", name.cyan()).bold()
+        );
+
+        let mut i = 0;
+        let mut line = String::from(" ");
+
+        while i < 20 + name.len() {
+            line.push('🭶');
+            i += 1;
+        }
+
+        format!("{}\n{}\n", output, line)
+    }
+
+    #[fixture]
+    fn earliest() -> String {
+        "   Earliest version: 0.1.0\n".to_string()
+    }
+
+    #[fixture]
+    fn highest_normal() -> String {
+        format!("   {}\n", "Highest normal version: 0.2.1".blue())
+    }
+
+    #[fixture]
+    fn highest() -> String {
+        format!("   {}\n", "Highest version: 0.2.1".green())
+    }
+
+    #[fixture]
+    fn recent() -> String {
+        format!("   {}\n", "Most recent version: 0.2.1".yellow())
+    }
+
+    #[fixture]
+    fn list() -> String {
+        "   \u{1b}[4m Yanked  Version \u{1b}[0m\n      \u{1b}[38;5;2m No\u{1b}[0m     0.1.0\n      \u{1b}[38;5;2m No\u{1b}[0m     0.1.1\n      \u{1b}[38;5;2m No\u{1b}[0m     0.1.3\n      \u{1b}[38;5;2m No\u{1b}[0m     0.2.1\n".to_string()
+    }
+
+    #[test]
+    fn test_run_earliest() {
+        let name = "some_crate";
+        let expected = format!("{}{}", header(name), &earliest());
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            earliest: true,
+            ..Default::default()
+        };
+
+        assert_eq!(crate_versions.crate_, "some_crate".to_string());
+        assert!(crate_versions.earliest);
+        assert!(!crate_versions.normal);
+        assert!(!crate_versions.highest);
+        assert!(!crate_versions.recent);
+        assert!(!crate_versions.list);
+        assert!(!crate_versions.all);
+        assert!(!crate_versions.key);
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_normal() {
+        let name = "some_crate";
+        let expected = format!("{}{}", header(name), &highest_normal());
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            normal: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_top() {
+        let name = "some_crate";
+        let expected = format!("{}{}", header(name), &highest());
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            highest: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_recent() {
+        let name = "some_crate";
+        let expected = format!("{}{}", header(name), &recent());
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            recent: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_list() {
+        let name = "some_crate";
+        let expected = format!("{}{}", header(name), &list());
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            list: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_all() {
+        let name = "some_crate";
+        let expected = format!(
+            "{}{}{}{}{}{}",
+            header(name),
+            &earliest(),
+            &highest_normal(),
+            &highest(),
+            &recent(),
+            &list()
+        );
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            all: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_key() {
+        let name = "some_crate";
+        let expected = format!(
+            "{}{}{}{}{}",
+            header(name),
+            &earliest(),
+            &highest_normal(),
+            &highest(),
+            &recent(),
+        );
+
+        let crate_versions = CrateVersions {
+            crate_: "some_crate".to_string(),
+            key: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_run_invalid_crate() {
+        let crate_versions = CrateVersions {
+            crate_: "some_non-existing_crate".to_string(),
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_run_invalid_crate_earliest() {
+        let crate_versions = CrateVersions {
+            crate_: "sdc_apis".to_string(),
+            earliest: true,
+            ..Default::default()
+        };
+
+        let result = crate_versions.run();
+        assert!(result.is_ok());
     }
 }
