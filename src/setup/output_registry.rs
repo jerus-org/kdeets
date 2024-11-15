52965a1fca5840a8fa74bbc543ad7750e07b51b0
Jeremiah Russell <jerry@jrussell.ie>
2024-11-15 17:00:22 +0000

    test: add unit tests for OutputRegistry enum variants

--- src/setup/output_registry.rs
+++ src/setup/output_registry.rs
@@ -5,3 +5,38 @@ pub(crate) enum OutputRegistry {
     Builder(LocalRegistryBuilder),
     Registry,
 }
+
+#[cfg(test)]
+mod tests {
+    use tame_index::PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn test_output_registry_none() {
+        let registry = OutputRegistry::None;
+        match registry {
+            OutputRegistry::None => (),
+            _ => panic!("Expected OutputRegistry::None variant"),
+        }
+    }
+
+    #[test]
+    fn test_output_registry_builder() {
+        let builder = LocalRegistryBuilder::create(PathBuf::from("test")).unwrap();
+        let registry = OutputRegistry::Builder(builder);
+        match registry {
+            OutputRegistry::Builder(_) => (),
+            _ => panic!("Expected OutputRegistry::Builder variant"),
+        }
+    }
+
+    #[test]
+    fn test_output_registry_registry() {
+        let registry = OutputRegistry::Registry;
+        match registry {
+            OutputRegistry::Registry => (),
+            _ => panic!("Expected OutputRegistry::Registry variant"),
+        }
+    }
+}
